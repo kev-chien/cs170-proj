d3ecd67d38e5b641bff7f94a6f58fd2002f38fb6
Kevin Chien <kevinchien17@gmail.com>
2018-04-30 12:47:04 -0700

    finished upper triangular matrix for TSP, delete python-christofides package

--- solver_phase2.py
+++ solver_phase2.py
@@ -5,6 +5,7 @@ sys.path.append('../..')
 import argparse
 import utils
 from student_utils_sp18 import *
+sys.path.insert(0, './python-christofides-0.1.2')
 from pyChristofides import christofides
 
 """
@@ -221,24 +222,29 @@ def christoTSP(list_of_kingdom_names, starting_kingdom, adjacency_matrix, conque
     # build TSP distance matrix to represent each node in conquered_kingdoms_indices
     # contain edges of weight based on shortest path
     G = adjacency_matrix_to_graph(adjacency_matrix)
-    shortest_paths = nx.algorithms.all_pairs_shortest_path(G)
-    shortest_lengths = nx.algorithms.all_pairs_shortest_path_length(G)
+    shortest_paths = dict(nx.shortest_path(G, weight="weight"))
+    shortest_lengths = dict(nx.shortest_path_length(G, weight="weight"))
 
     N_TSP = len(conquered_kingdoms_indices)
     dict_TSP_index_to_index = { TSP_index: index for TSP_index, index in enumerate(conquered_kingdoms_indices)}
     dict_index_to_TSP_index = { index: TSP_index for TSP_index, index in enumerate(conquered_kingdoms_indices)}
 
-    distance_matrix = [[0 for _ in range(N_TSP)] for _ in range(N_TSP)]
+    distance_matrix = [[0] * N_TSP] * N_TSP
+    print(N_TSP)
+
+    print(shortest_lengths)
+    print(distance_matrix)
 
     for i in range(N_TSP):
         for j in range(i + 1, N_TSP):
+            print(i, j, shortest_lengths[i][j])
             distance_matrix[i][j] = shortest_lengths[i][j]
     closed_walk = []
 
+    print(distance_matrix)
+
     # run christofides
-    # NOTE: to install pyChristofides, cd in to python-christofides, run `python3 setup.py install`
-    TSP = christofides.compute(distance_matrix)
-    print(TSP['Christofides_Solution'])
+
     return closed_walk
 
 """
