1e46088ecc277ac8faf97ab27224a0b8d591165b
Kevin Chien <kevinchien17@gmail.com>
2018-04-30 22:43:29 -0700

    shell script work pls

--- run_docker_all.sh
+++ run_docker_all.sh
@@ -2,5 +2,5 @@
 for i in {0..752}
 do
   docker run tspsolver "in/${i}.in" outputs-concorde dijkstras-greedy concorde-TSP > "outputs-concorde/${i}.out"
-  docker rm $(docker ps -a -q)
+  docker ps -a -q
 done
