7c0cc093248da59a5797677d4e2bf05c5c460f96
Kevin Chien <kevinchien17@gmail.com>
2018-04-30 22:37:28 -0700

    updated script so automatically removes containers

--- run_docker_all.sh
+++ run_docker_all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 for i in {0..752}
 do
-   docker run tspsolver "in/{i}.in" outputs-concorde dijkstras-greedy concorde-TSP > "outputs-concorde/{i}.out"
+  docker run tspsolver "in/${i}.in" outputs-concorde dijkstras-greedy concorde-TSP > "outputs-concorde/${i}.out"
+  docker rm $(docker ps -a -q)
 done
